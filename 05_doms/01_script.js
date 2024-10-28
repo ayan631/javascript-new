308e6f9d71731d049fc1ab18ab11436050d66ee7
ayan631 <bhattacherjeeayan60@gmail.com>
2024-10-28 07:19:34 +0000

    done!

--- 05_doms/01_script.js
+++ 05_doms/01_script.js
@@ -1,4 +1,5 @@
-// alert("Ready?")
+console.log("Documen is Loaded!");
+
 // getElementById:-
 document.getElementById('heading1').innerHTML='DOM Manipulation'
 document.getElementById('heading2').innerHTML='By JavaScript'
@@ -10,8 +11,18 @@ const items = Array.from(document.getElementsByClassName('items'))
 items.forEach((i) => i.style = 'color: green')
 
 
+//get the tagName of any element:
+console.log(document.querySelector('#heading1').tagName)
+
+
+// fristChild and lastChild and children:
+console.log(document.querySelector('.container').firstChild);
+console.log(document.querySelector('.container').lastChild);
+console.log(document.querySelector('.container').children);
+
+
+
 // make an event to change the theme:
-console.log("Documen is Loaded!");
 document.querySelector('button').addEventListener('click', function(e){
     document.querySelector('body').style.color = '#212121';
     document.querySelector('body').style.backgroundColor = '#fff'; 
