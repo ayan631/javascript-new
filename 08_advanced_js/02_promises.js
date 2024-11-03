99b384fd14b042f83aa4e3bcd9ed24e866ba3031
ayan631 <bhattacherjeeayan60@gmail.com>
2024-11-03 12:30:41 +0000

    promises done!

--- 08_advanced_js/02_promises.js
+++ 08_advanced_js/02_promises.js
@@ -1,3 +1,4 @@
+// first promise:
 let promise1 = new Promise(function (resolve, reject) {
     // Do an asynchronous task: DB calls, file read, Network call etc...
     setTimeout(function(){
@@ -9,9 +10,61 @@ let promise1 = new Promise(function (resolve, reject) {
 promise1.then(function() {
     console.log('Promise consumed !');
 })
-.catch(function (error){
-    console.log(error);
+
+
+// second promise:
+let promise2 = new Promise(function (resolve, reject) {
+    setTimeout(function() {
+        resolve({username: 'ayan', useremail: 'ayan@example.com'}) 
+    },2000);
 })
-.finally(function (){
-    console.log("This code will be executed regardless of the state of promises.");
+
+promise2.then(function (user) {
+    console.log(`Name = ${user.username};  Email = ${user.useremail}`);
 })
+
+
+// third promise:
+let promise3 = new Promise((resolve, reject)=>{
+    setTimeout(() => {
+        let error = true
+        if (!error) {
+            resolve('Database is connected successfully !!')
+        }
+        else {
+            reject('Oops! Something went wrong !')
+        }
+    }, 2000)
+}).then((result) => {
+    console.log(result)
+}).catch((error) => {
+    console.log(error)
+}).finally(() => {
+    console.log("The promise is either resolved or rejected.")    
+})
+
+
+
+// Fourth promise:
+let promise4 = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        let err = true
+        if(!err){
+            resolve({username:'ayan', password:'12345'})
+        }else{
+            reject('ERROR: JS went wrong!')
+        }
+    }, 2000)
+})
+
+// we can also use async-await keywords to handle resolved promises but the only thing is we can not handle rejected promises directly. So, use try-catch block.
+async function consumePromise4() {
+    try{
+        const response = await promise4; //because promise4 is an object
+        console.log(response)
+    }catch(error){
+        console.log(error)
+    } 
+}
+
+consumePromise4()
